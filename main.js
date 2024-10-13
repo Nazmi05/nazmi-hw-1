92f3435ac0e07103be45fc0a6609e70900a1cc7a
Nazmi05 <narikoscarlet@gmail.com>
2024-10-13 19:02:04 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+/*const a=document.querySelector(".nubmer-1")
+ const b=document.querySelector(".nubmer-2")
+ const plus=document.querySelector(".plus")
+ const minus= document.querySelector(".minus")
+ const multiplication= document.querySelector(".multiplication")
+const division= document.querySelector(".division")
+ const res__btn=document.querySelector(".res__btn")
+ const res=document.querySelector(".res")
+
+ console.log(a);
+ console.log(b);
+ console.log(plus);
+ console.log(minus);
+ console.log(multiplication);
+ console.log(division);
+ console.log(res__btn);
+ console.log(res);
+ 
+ plus.addEventListener('click', function(){
+   res.innerHTML=Number(a.value)+Number(b.value);
+ })
+ minus.addEventListener('click', function(){
+   res.innerHTML=Number(a.value)-Number(b.value);
+ })
+ multiplication.addEventListener('click', function(){
+   res.innerHTML=Number(a.value)*Number(b.value)
+ })
+ division.addEventListener('click', function(){
+   res.innerHTML=Number(a.value)/Number(b.value)
+ })
+
+*/
+/*
+const inputField=document.querySelector("#inputField")
+const input__btn=document.querySelector(".input__btn")
+console.log(inputField);
+console.log(input__btn);
+
+
+input__btn.addEventListener('click',function(){
+    inputField.value="Привет мир!";
+ })
+*/
+/*
+ const click__btn = document.querySelector(".click__button");
+const clk__num = document.querySelector(".clk__number");
+
+console.log(click__btn);
+console.log(clk__num);
+
+let count = 0;  
+
+click__btn.addEventListener('click', function() {
+    count += 1;  
+    clk__num.innerHTML = count; 
+});
+
+ */
+/*
+ const null__input=document.querySelector(".null__input")
+ const btn__checker=document.querySelector(".btn__checker")
+const display=document.querySelector(".display")
+
+ console.log (null__input);
+ console.log(btn__checker);
+ console.log(display);
+
+ 
+ btn__checker.addEventListener('click',function(){
+    if(null__input.value !==""){
+        display.innerText="Checked";
+    }
+    else{
+        display.innerText="Mistake";
+    }
+    })
+*/
+/*
+ const shift=document.querySelector(".shift")  
+ const text=document.querySelector(".text")
+
+ console.log(shift);
+ console.log(text);
+
+  shift.addEventListener('click',function(){
+    text.style.fontSize ='26px';
+    text.style.fontWeight ='bold';
+    
+  })*/
